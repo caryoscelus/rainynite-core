b1849f3d6dfd829151af94b51afb26f68ef187a9
caryoscelus <caryoscelus@gmx.com>
2017-03-29 13:36:45 +0300

    Serializing: initial commit

---
+++ include/core/serialize.h
@@ -0,0 +1,182 @@
+/*
+ *  serialize.h - node tree (de)serializer
+ *  Copyright (C) 2017 caryoscelus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __CORE__SERIALIZE_H__821FEF22
+#define __CORE__SERIALIZE_H__821FEF22
+
+#include <set>
+#include <ostream>
+
+namespace core {
+namespace serialize {
+
+enum class RecordType {
+    Value,
+    List,
+    Map
+};
+
+template <typename U>
+class Writer;
+
+template <typename U, class T>
+void put_value(Writer<U>& serializer, T const& object);
+
+template <typename U, class T>
+void put_list(Writer<U>& serializer, T const& object);
+
+template <typename U, class T>
+void put_map(Writer<U>& serializer, T const& object);
+
+template <typename U, class T>
+U get_reference(T const& object);
+
+template <typename U>
+std::string id_to_string(U id);
+
+template <class T>
+std::string get_type(T const& object);
+
+template <class T>
+RecordType classify(T const& object);
+
+template <typename U>
+class Writer {
+public:
+    template <class T>
+    void object(T const& object) {
+        auto ref = get_reference<U>(object);
+        if (is_stored(ref)) {
+            reference(ref);
+        } else {
+            store_object(ref);
+            object_start();
+            type(get_type(object));
+            switch (classify(object)) {
+                case RecordType::Value:
+                    object_value_start();
+                    put_value(*this, object);
+                    object_value_end();
+                    break;
+                case RecordType::List:
+                    object_value_start();
+                    put_list(*this, object);
+                    object_value_end();
+                    break;
+                case RecordType::Map:
+                    put_map(*this, object);
+                    break;
+            }
+            object_end();
+        }
+    }
+    virtual void object_start() = 0;
+    virtual void object_end() = 0;
+    virtual void object_value_start() = 0;
+    virtual void object_value_end() = 0;
+    virtual void list_start() = 0;
+    virtual void list_end() = 0;
+    virtual void type(std::string const& s) = 0;
+    virtual void string(std::string const& s) = 0;
+    virtual void key(std::string const& s) = 0;
+    virtual void number(double x) = 0;
+    virtual void reference(U id) = 0;
+private:
+    std::set<U> storage;
+    bool is_stored(U id) {
+        return storage.count(id);
+    }
+    void store_object(U id) {
+        storage.insert(id);
+    }
+};
+
+template <typename U>
+class DumbJsonWriter : public Writer<U> {
+public:
+    DumbJsonWriter(std::ostream& stream_) :
+        stream(stream_)
+    {}
+public:
+    virtual void object_start() override {
+        element();
+        write("{");
+        prev_element = false;
+    }
+    virtual void object_end() override {
+        write("}");
+        prev_element = true;
+    }
+    virtual void object_value_start() override {
+        key("VALUE");
+    }
+    virtual void object_value_end() override {
+    }
+    virtual void list_start() override {
+        write("[");
+        prev_element = false;
+    }
+    virtual void list_end() override {
+        write("]");
+        prev_element = true;
+    }
+    virtual void type(std::string const& s) override {
+        key("TYPE");
+        write_string(s);
+        element();
+    }
+    virtual void string(std::string const& s) override {
+        element();
+        write_string(s);
+    }
+    virtual void key(std::string const& s) override {
+        element();
+        write_string(s);
+        write(":");
+        prev_element = false;
+    }
+    virtual void number(double x) override {
+        element();
+        write(std::to_string(x));
+    }
+    virtual void reference(U id) override {
+        string(id_to_string(id));
+    }
+private:
+    void element() {
+        if (prev_element)
+            write(",");
+        prev_element = true;
+    }
+    void write(std::string const& s) {
+        stream << s;
+    }
+    void write_string(std::string const& s) {
+        write("\"");
+        write(s);
+        write("\"");
+    }
+private:
+    std::ostream& stream;
+    bool prev_element = false;
+};
+
+} // namespace serialize
+} // namespace core
+
+#endif
---
+++ tests/serialize.cpp
@@ -0,0 +1,144 @@
+/*
+ *  serialize.cpp - serializing tests
+ *  Copyright (C) 2017 caryoscelus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <catch.hpp>
+
+#include <iostream>
+
+#include <boost/uuid/uuid_io.hpp>
+
+#include <core/serialize.h>
+#include <core/node.h>
+#include <core/class_init.h>
+
+using namespace core;
+
+namespace core {
+
+class ValueToString {
+public:
+    virtual std::string operator()(boost::any const& object) const = 0;
+};
+
+template <class T>
+class AutoValueToString : public ValueToString, class_init::Registered<AutoValueToString<T>, T, ValueToString> {
+public:
+    virtual std::string operator()(boost::any const& object) const override {
+        auto value = boost::any_cast<T>(object);
+        return std::to_string(value);
+    }
+};
+
+template class AutoValueToString<double>;
+
+class TypeName {
+public:
+    virtual std::string operator()() const = 0;
+};
+
+class DoubleTypeName : public TypeName, class_init::Registered<DoubleTypeName, double, TypeName> {
+public:
+    virtual std::string operator()() const override {
+        return "double";
+    }
+};
+
+class ValueTypeNameBase {
+public:
+    virtual std::string operator()(boost::any const& object) const = 0;
+};
+
+class ValueTypeName : public ValueTypeNameBase, class_init::Registered<ValueTypeName, std::shared_ptr<AbstractValue>, ValueTypeNameBase> {
+public:
+    virtual std::string operator()(boost::any const& object) const {
+        auto value = boost::any_cast<std::shared_ptr<AbstractValue>>(object);
+        return class_init::type_info<TypeName, std::string>(value->get_type());
+    }
+};
+
+namespace serialize {
+
+template <>
+void put_value(Writer<Id>& writer, std::shared_ptr<AbstractValue> const& object) {
+    writer.string(class_init::any_info<ValueToString, std::string>(object->any()));
+}
+
+template <>
+void put_list(Writer<Id>& writer, std::shared_ptr<AbstractValue> const& object) {
+    //
+}
+
+template <>
+void put_map(Writer<Id>& writer, std::shared_ptr<AbstractValue> const& object) {
+    //
+}
+
+template <>
+Id get_reference(std::shared_ptr<AbstractValue> const& object) {
+    return object->get_id();
+}
+
+template <>
+std::string id_to_string(Id id) {
+    return to_string(id);
+}
+
+template <>
+std::string get_type(std::shared_ptr<AbstractValue> const& object) {
+    try {
+        return class_init::any_info<ValueTypeNameBase,std::string>(object);
+    } catch (class_init::RuntimeTypeError const& ex) {
+        return "unknown";
+    }
+}
+
+template <>
+RecordType classify(std::shared_ptr<AbstractValue> const& object) {
+    if (object->is_const())
+        return RecordType::Value;
+    return RecordType::Map;
+}
+
+}
+}
+
+class Add : public Node<double> {
+public:
+    Add() {
+        init<double>(a, 0);
+        init<double>(b, 0);
+    }
+public:
+    virtual double get(Time t) const override {
+        auto a = get_a()->get(t);
+        auto b = get_b()->get(t);
+        return a+b;
+    }
+
+    NODE_PROPERTY(a, double);
+    NODE_PROPERTY(b, double);
+};
+
+TEST_CASE("Dumb json serialize", "[serialize,node]") {
+    CHECK(true);
+    auto writer = serialize::DumbJsonWriter<Id>(std::cout);
+    std::shared_ptr<AbstractValue> three = make_value<double>(3);
+    writer.object(three);
+    std::shared_ptr<AbstractValue> add = std::make_shared<Add>();
+    writer.object(add);
+}
