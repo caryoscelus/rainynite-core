71d50b192785df2b09f19d92e95e462ace4ad8ed
caryoscelus <caryoscelus@gmx.com>
2017-03-29 12:18:08 +0300

    Class init / runtime info helpers

---
+++ include/core/class_init.h
@@ -0,0 +1,96 @@
+/*
+ *  class_init.h - helper templates for runtime type info
+ *  Copyright (C) 2017 caryoscelus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __CORE__CLASS_INIT_H__9F78DE78
+#define __CORE__CLASS_INIT_H__9F78DE78
+
+#include <typeindex>
+#include <map>
+
+#include <boost/any.hpp>
+
+namespace class_init {
+
+/**
+ * Automatic class initialization helper.
+ *
+ * To use this class, inherit from it in a "curiously returning template
+ * pattern" (e.g. `class Foo : Initialized < Foo >`) and define a public
+ * static init() function - it will be launch on init (i.e. on static
+ * initialization stage - before main() in executable or on load of dynamic
+ * library.
+ */
+template <class T>
+class Initialized {
+private:
+    struct Init {
+        Init() {
+            T::init();
+        }
+    };
+    static Init instance;
+    template <Init*> struct RR {};
+    static RR<&instance> r_insatnce;
+};
+
+template <typename T>
+typename Initialized<T>::Init Initialized<T>::instance;
+
+template <class T>
+std::map<std::type_index, T*>& class_registry() {
+    static std::map<std::type_index, T*> instance;
+    return instance;
+}
+
+/**
+ * Automatic class registration helper.
+ *
+ * This class template (along with class_registry() template) allows to
+ * create automatically-registered binding between a type and some value
+ * which can then be retrieved in runtime.
+ *
+ * Usage:
+ * - Create an (abstract) Base class which will hold required type
+ *   information and interface to retrieve it
+ * - For each type, create public Base SubClass which also inherits from
+ *   Registered< SubClass, TargetType, Base >
+ * - Retrieve information by querying class_registry() with runtime type
+ *   information (type_index or type_info which converts to it).
+ */
+template <class S, class T, class R>
+class Registered : public Initialized<Registered<S, T, R>> {
+public:
+    static void init() {
+        class_registry<R>()[typeid(T)] = new S();
+    }
+};
+
+template <class T, class R>
+R type_info(std::type_index type) {
+    return (*class_registry<T>().at(type))();
+}
+
+template <class T, class R>
+R any_info(boost::any const& object) {
+    std::type_index type = object.type();
+    return (*class_registry<T>().at(type))(object);
+}
+
+}
+
+#endif
---
+++ tests/class_init.cpp
@@ -0,0 +1,93 @@
+/*
+ *  clsas_init.cpp - class init tests
+ *  Copyright (C) 2017 caryoscelus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <catch.hpp>
+
+#include <core/class_init.h>
+
+using namespace class_init;
+
+bool& foo_registered() {
+    static bool instance = false;
+    return instance;
+}
+
+class Foo : Initialized<Foo> {
+public:
+    static void init() {
+        foo_registered() = true;
+    }
+};
+
+TEST_CASE("Automatic class initialization", "") {
+    CHECK(foo_registered());
+}
+
+class Bar {
+};
+
+class ClassName {
+public:
+    virtual std::string operator()() const = 0;
+};
+
+class BarName : public ClassName, Registered<BarName, Bar, ClassName> {
+public:
+    virtual std::string operator()() const override {
+        return "Bar";
+    }
+};
+
+TEST_CASE("Automatic class registration", "") {
+    CHECK((type_info<ClassName, std::string>(typeid(Bar)) == "Bar"));
+}
+
+class FooBar {
+public:
+    virtual std::string name() const {
+        return "FooBar";
+    }
+};
+
+class FooBaz : public FooBar {
+public:
+    virtual std::string name() const override {
+        return "FooBaz";
+    }
+};
+
+class PolyClassName {
+public:
+    virtual std::string operator()(boost::any const& object) const = 0;
+};
+
+class FooBarName : public PolyClassName, Registered<FooBarName, FooBar*, PolyClassName> {
+public:
+    virtual std::string operator()(boost::any const& object) const override {
+        auto p = boost::any_cast<FooBar*>(object);
+        return p->name();
+    }
+};
+
+TEST_CASE("Polymorphic class name", "") {
+    FooBar* obj = new FooBar();
+    CHECK((any_info<PolyClassName, std::string>(obj) == "FooBar"));
+    delete obj;
+    obj = new FooBaz();
+    CHECK((any_info<PolyClassName, std::string>(obj) == "FooBaz"));
+}
