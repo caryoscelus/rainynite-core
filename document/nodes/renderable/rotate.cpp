99904f4ab047c9deebdc9e7faab6e11c8b8137b2
caryoscelus <caryoscelus@gmx.com>
2017-06-08 14:53:37 +0300

    Rotate node

---
+++ document/nodes/renderable/rotate.cpp
@@ -0,0 +1,45 @@
+/*
+ *  rotate.cpp - Rotate transformation render node
+ *  Copyright (C) 2017 caryoscelus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <core/renderable.h>
+#include <core/node_info.h>
+
+#include <2geom/point.h>
+
+namespace core {
+namespace nodes {
+
+class Rotate : public RenderableNode {
+public:
+    Rotate() {
+        init<Renderable>(source, {});
+        init<double>(angle, {});
+    }
+    virtual std::string get_source_name() const override {
+        return "source";
+    }
+
+private:
+    NODE_PROPERTY(source, Renderable);
+    NODE_PROPERTY(angle, double);
+};
+
+REGISTER_NODE(Rotate);
+
+} // namespace nodes
+} // namespace core
--- renderers/svg_renderer/transform.cpp
+++ renderers/svg_renderer/transform.cpp
@@ -29,6 +29,7 @@ namespace renderers {
 
 const std::string svg_translate = R"svg(<g transform="translate({}, {})">{}</g>)svg";
 const std::string svg_scale = R"svg(<g transform="scale({x}, {y})">{source}</g>)svg";
+const std::string svg_rotate = R"svg(<g transform="rotate({angle})">{source}</g>)svg";
 
 class TranslateSvgRenderer : SVG_RENDERER_MODULE_CLASS(TranslateSvgRenderer) {
     SVG_RENDERER_MODULE_NAME("Translate");
@@ -50,6 +51,20 @@ public:
             svg_scale,
             "x"_a=scale.x(),
             "y"_a=scale.y(),
+            "source"_a=node_to_svg(source, time, settings)
+        );
+    }
+};
+
+class RotateSvgRenderer : SVG_RENDERER_MODULE_CLASS(RotateSvgRenderer) {
+    SVG_RENDERER_MODULE_NAME("Rotate");
+public:
+    virtual std::string operator()(AbstractNode const& node, Time time, SvgRendererSettings const& settings) const override {
+        auto source = node.get_property("source");
+        auto angle = node.get_property_as<double>("angle")->get(time);
+        return fmt::format(
+            svg_rotate,
+            "angle"_a=angle,
             "source"_a=node_to_svg(source, time, settings)
         );
     }
