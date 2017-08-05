caryoscelus/rainynite-core

commit ab6fc6484b39ee13263e1a2ed10e5b457124de07

Minor fix in bezier morph node

document/nodes/morph.cpp
@@ -35,14 +35,14 @@ public:
         init<double>(progress, 0);
     }
 public:
-    virtual Geom::BezierKnots get(Time time) const override {
+    Geom::BezierKnots get(Time time) const override {
         try {
             auto a = get_a()->get(time);
             auto b = get_b()->get(time);
             if (a != cached_a || b != cached_b) {
+                morphing::prepare_average(cached_a, cached_b, avg_a, avg_b);
                 cached_a = a;
                 cached_b = b;
-                morphing::prepare_average(cached_a, cached_b, avg_a, avg_b);
             }
             auto t = get_progress()->get(time);
             return morphing::simple_average(avg_a, avg_b, t);

commit 0ccc582562a35e621cd3b85ec076b3a73c001cea

Fix bezier morph node
Fixes bug introduced in ab6fc6484b39ee13263e1a2ed10e5b457124de07

document/nodes/morph.cpp
@@ -40,7 +40,7 @@ public:
             auto a = get_a()->get(time);
             auto b = get_b()->get(time);
             if (a != cached_a || b != cached_b) {
-                morphing::prepare_average(cached_a, cached_b, avg_a, avg_b);
+                morphing::prepare_average(a, b, avg_a, avg_b);
                 cached_a = a;
                 cached_b = b;
             }