ab6fc6484b39ee13263e1a2ed10e5b457124de07
caryoscelus <caryoscelus@gmx.com>
2017-08-05 13:56:36 +0300

    Minor fix in bezier morph node

--- document/nodes/morph.cpp
+++ document/nodes/morph.cpp
@@ -35,14 +35,14 @@ public:
         init<double>(progress, 0);
     }
 public:
-    virtual Geom::BezierKnots get(Time time) const override {
+    Geom::BezierKnots get(Time time) const override {
         try {
             auto a = get_a()->get(time);
             auto b = get_b()->get(time);
             if (a != cached_a || b != cached_b) {
+                morphing::prepare_average(cached_a, cached_b, avg_a, avg_b);
                 cached_a = a;
                 cached_b = b;
-                morphing::prepare_average(cached_a, cached_b, avg_a, avg_b);
             }
             auto t = get_progress()->get(time);
             return morphing::simple_average(avg_a, avg_b, t);
