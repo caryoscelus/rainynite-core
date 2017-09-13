caryoscelus/rainynite-core

commit 4ef5eb953d3b6b175cfe3db603824b096b9b280c

Re-import common types from `std` & `boost` into `rainynite` ns
- std::vector
- std::map
- std::string (including function rename)
- std::{shared,unique}_ptr
- std::type_index (as Type)
- boost::any (including function rename)
- boost::optional -> std::optional (this involves replacing none with {})

ref #11

include/core/serialize/json.h
@@ -59,16 +59,16 @@ public:
         write("]");
         prev_element = true;
     }
-    void type(std::string const& s) override {
+    void type(string const& s) override {
         key("TYPE");
         write_string(s);
         element();
     }
-    void string(std::string const& s) override {
+    void value_string(string const& s) override {
         element();
         write_string(s);
     }
-    void key(std::string const& s) override {
+    void key(string const& s) override {
         element();
         write_string(s);
         write(":");
@@ -87,17 +87,17 @@ private:
             write(",\n");
         prev_element = true;
     }
-    void write(std::string const& s) {
+    void write(string const& s) {
         stream << s;
     }
-    std::string escape(std::string const& s) {
-        std::string r = s;
+    string escape(string const& s) {
+        string r = s;
         boost::replace_all(r, "\\", "\\\\");
         boost::replace_all(r, "\"", "\\\"");
         boost::replace_all(r, "\n", "\\n");
         return r;
     }
-    void write_string(std::string const& s) {
+    void write_string(string const& s) {
         write("\"");
         write(escape(s));
         write("\"");

commit 71efa93705cfff4a7da920b6e4cc1e951df8bd4f

Fix JSON serialization
Bug was introduced by 4ef5eb953d3b6b175cfe3db603824b096b9b280c

include/core/serialize/json.h
@@ -40,7 +40,7 @@ public:
         write("{");
         prev_element = false;
         key("UID");
-        string(V::id_to_string(id));
+        value_string(V::id_to_string(id));
     }
     void object_end() override {
         write("}");
@@ -79,7 +79,7 @@ public:
         write(std::to_string(x));
     }
     void reference(U id) override {
-        string(V::id_to_string(id));
+        value_string(V::id_to_string(id));
     }
 private:
     void element() {