7e285713fae4df047f9f1118c579755df1ae37d4
caryoscelus <caryoscelus@gmx.com>
2017-09-14 21:26:45 +0300

    Abstract Factory template interface

---
+++ include/core/abstract_factory.h
@@ -0,0 +1,34 @@
+/*
+ *  abstract_factory.h - Abstract Factory interface template
+ *  Copyright (C) 2017 caryoscelus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CORE_ABSTRACT_FACTORY_H_302C0197_C085_5571_A533_C4B637493FFB
+#define CORE_ABSTRACT_FACTORY_H_302C0197_C085_5571_A533_C4B637493FFB
+
+#include <core/std/memory.h>
+
+namespace rainynite {
+
+template <typename T>
+class AbstractFactory {
+public:
+    virtual unique_ptr<T> operator()() = 0;
+};
+
+} // namespace rainynite
+
+#endif
