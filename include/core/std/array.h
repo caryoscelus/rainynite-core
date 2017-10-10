cd6e1a5df842793a9b8845ecf13e5fa70d8b37da
caryoscelus <caryoscelus@gmx.com>
2017-10-10 18:22:53 +0300

    std/array.h

---
+++ include/core/std/array.h
@@ -0,0 +1,29 @@
+/*  std/array.h - re-import array
+ *  Copyright (C) 2017 caryoscelus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CORE_STD_ARRAY_H_8AE53A0E_9AF5_5E0C_AFA9_62D1EBE020B1
+#define CORE_STD_ARRAY_H_8AE53A0E_9AF5_5E0C_AFA9_62D1EBE020B1
+
+#include <array>
+
+namespace rainynite {
+
+using std::array;
+
+} // namespace rainynite
+
+#endif
