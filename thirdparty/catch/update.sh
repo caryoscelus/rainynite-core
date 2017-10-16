47d104d2f2904b4944217abd82a6420b8a3066bf
caryoscelus <caryoscelus@gmx.com>
2017-10-17 01:58:31 +0300

    Catch update script fix
    Overwrite file instead of creating .1 copies

--- thirdparty/catch/update.sh
+++ thirdparty/catch/update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget https://raw.githubusercontent.com/philsquared/Catch/master/single_include/catch.hpp
+wget -r https://raw.githubusercontent.com/philsquared/Catch/master/single_include/catch.hpp
