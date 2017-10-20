2cada6c7e63a6da03519071b4e0f8a5b58998b8c
caryoscelus <caryoscelus@gmx.com>
2017-10-20 19:45:31 +0300

    SvgRenderer: remove support of deprecated nodes

--- renderers/svg_renderer/transform.cpp
+++ renderers/svg_renderer/transform.cpp
@@ -26,9 +26,6 @@ using namespace fmt::literals;
 
 namespace rainynite::core::renderers {
 
-const string svg_translate = R"svg(<g transform="translate({}, {})">{}</g>)svg";
-const string svg_scale = R"svg(<g transform="scale({x}, {y})">{source}</g>)svg";
-const string svg_rotate = R"svg(<g transform="rotate({angle}, {x}, {y})">{source}</g>)svg";
 const string svg_matrix = R"svg(<g transform="{matrix_2x3}">{source}</g>)svg";
 
 class TransformSvgRenderer : SVG_RENDERER_MODULE_CLASS(TransformSvgRenderer) {
@@ -40,48 +37,6 @@ public:
         return fmt::format(
             svg_matrix,
             "matrix_2x3"_a=serialize::value_to_string(matrix),
-            "source"_a=node_to_svg({source, ctx}, settings)
-        );
-    }
-};
-
-class TranslateSvgRenderer : SVG_RENDERER_MODULE_CLASS(TranslateSvgRenderer) {
-    SVG_RENDERER_MODULE_NAME("Translate");
-public:
-    string operator()(AbstractNode const& node, shared_ptr<Context> ctx, SvgRendererSettings const& settings) const override {
-        auto source = node.get_property("source");
-        auto offset = node.get_property_as<Geom::Point>("offset")->get(ctx);
-        return fmt::format(svg_translate, offset.x(), offset.y(), node_to_svg({source, ctx}, settings));
-    }
-};
-
-class ScaleSvgRenderer : SVG_RENDERER_MODULE_CLASS(ScaleSvgRenderer) {
-    SVG_RENDERER_MODULE_NAME("Scale");
-public:
-    string operator()(AbstractNode const& node, shared_ptr<Context> ctx, SvgRendererSettings const& settings) const override {
-        auto source = node.get_property("source");
-        auto scale = node.get_property_as<Geom::Point>("scale")->get(ctx);
-        return fmt::format(
-            svg_scale,
-            "x"_a=scale.x(),
-            "y"_a=scale.y(),
-            "source"_a=node_to_svg({source, ctx}, settings)
-        );
-    }
-};
-
-class RotateSvgRenderer : SVG_RENDERER_MODULE_CLASS(RotateSvgRenderer) {
-    SVG_RENDERER_MODULE_NAME("Rotate");
-public:
-    string operator()(AbstractNode const& node, shared_ptr<Context> ctx, SvgRendererSettings const& settings) const override {
-        auto source = node.get_property("source");
-        auto angle = node.get_property_as<double>("angle")->get(ctx);
-        auto origin = node.get_property_as<Geom::Point>("origin")->get(ctx);
-        return fmt::format(
-            svg_rotate,
-            "angle"_a=angle*360,
-            "x"_a=origin.x(),
-            "y"_a=origin.y(),
             "source"_a=node_to_svg({source, ctx}, settings)
         );
     }
