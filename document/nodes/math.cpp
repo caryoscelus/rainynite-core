13379d68a5e1abfa4484b7ea876fa9cfddfce440
caryoscelus <caryoscelus@gmx.com>
2017-11-01 20:49:03 +0300

    Remove non-generic Add nodes

--- document/nodes/math.cpp
+++ document/nodes/math.cpp
@@ -1,5 +1,4 @@
-/*
- *  math.cpp - math operation nodes
+/*  math.cpp - math operations nodes
  *  Copyright (C) 2017 caryoscelus
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -20,59 +19,13 @@
 
 #include <boost/math/constants/constants.hpp>
 
-#include <2geom/point.h>
-
 #include <core/node_info.h>
 #include <core/node/node.h>
 #include <core/node/property.h>
 
 using boost::math::double_constants::pi;
 
-namespace rainynite::core {
-namespace nodes {
-
-class Add : public Node<double> {
-public:
-    Add() {
-        init<double>(a, 0);
-        init<double>(b, 0);
-    }
-public:
-    double get(shared_ptr<Context> ctx) const override {
-        try {
-            return get_a()->get(ctx) + get_b()->get(ctx);
-        } catch (...) {
-            return {};
-        }
-    }
-
-private:
-    NODE_PROPERTY(a, double);
-    NODE_PROPERTY(b, double);
-};
-REGISTER_NODE(Add);
-
-// TODO: generic Add node
-class PointAdd : public Node<Geom::Point> {
-public:
-    PointAdd() {
-        init<Geom::Point>(a, {});
-        init<Geom::Point>(b, {});
-    }
-public:
-    Geom::Point get(shared_ptr<Context> ctx) const override {
-        try {
-            return get_a()->get(ctx) + get_b()->get(ctx);
-        } catch (...) {
-            return {};
-        }
-    }
-
-private:
-    NODE_PROPERTY(a, Geom::Point);
-    NODE_PROPERTY(b, Geom::Point);
-};
-REGISTER_NODE(PointAdd);
+namespace rainynite::core::nodes {
 
 class Multiply : public Node<double> {
 public:
@@ -114,5 +67,4 @@ private:
 };
 REGISTER_NODE(Sin);
 
-} // namespace nodes
-} // namespace rainynite::core
+} // namespace rainynite::core::nodes
