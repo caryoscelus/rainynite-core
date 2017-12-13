79560cd9914f431171cdb3c4c24f6e707c402fb9
caryoscelus <caryoscelus@gmx.com>
2017-12-13 14:25:40 +0300

    Yaml writer: rewrite using node tree

--- filters/yaml_writer.cpp
+++ filters/yaml_writer.cpp
@@ -1,5 +1,4 @@
-/*
- *  yaml_writer.cpp - yaml serializer
+/*  filters/yaml_writer.cpp - yaml serializer
  *  Copyright (C) 2017 caryoscelus
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -23,127 +22,117 @@
 #include <core/document.h>
 #include <core/state_machine.h>
 #include <core/serialize/node_writer.h>
+#include <core/node_info.h>
+#include <core/node_tree_traverse.h>
 #include <core/filters/yaml_writer.h>
 
 using namespace fmt::literals;
 
-namespace rainynite::core::serialize {
+namespace rainynite::core::filters {
 
 enum class YamlCppWriterState {
     DocumentStart,
-    ObjectStart,
-    TypedObject,
-    AwaitValue,
-    ValueObject,
     Map,
     List,
 };
 
-template <class V, typename U>
-class YamlCppWriter : public Writer<V, U>, protected StateMachine<YamlCppWriterState> {
+class YamlCppWriter :
+    public TreeTraverser,
+    protected StateMachine<YamlCppWriterState>
+{
     using State = YamlCppWriterState;
 public:
     YamlCppWriter(std::ostream& stream) :
         emitter(stream)
     {
+    }
+
+    void serialize(shared_ptr<Document> document) {
         push_state(State::DocumentStart);
         emitter << YAML::BeginDoc;
+        tree = document->get_tree();
+        traverse_tree(tree);
     }
 
-    void object_start(U id) override {
-        emitter << YAML::Anchor(V::id_to_string(id));
-        push_state(State::ObjectStart);
-    }
-    void object_end() override {
+    bool object_start() override {
         if (state() == State::Map) {
-            emitter << YAML::EndMap;
-            pop_state(State::Map);
-        } else if (state() == State::ValueObject) {
-            pop_state(State::ValueObject);
-        } else if (state(1) == State::Map) {
-            emitter << YAML::Value << YAML::BeginMap << YAML::EndMap;
+            emitter << YAML::Key << current().key;
         }
-        pop_state(State::TypedObject);
-    }
-    void object_value_start() override {
-        push_state(State::AwaitValue);
-    }
-    void object_value_end() override {
-        pop_state(State::AwaitValue);
-        push_state(State::ValueObject);
-    }
-    void list_start() override {
-        expect_state(State::AwaitValue);
-        push_state(State::List);
-        emitter << YAML::BeginSeq;
-    }
-    void list_end() override {
-        emitter << YAML::EndSeq;
-        pop_state(State::List);
-    }
-    void type(string const& s) override {
-        expect_state(State::ObjectStart);
-        if (!(optional_type && s.find("Value/")==0)) {
-            emitter << YAML::LocalTag(s);
-        }
-        optional_type = false;
-        set_state(State::TypedObject);
-    }
-    void auto_type() override {
-        throw SerializationError("Not supported at the moment");
-    }
-    void next_type_is_optional(bool opt) override {
-        optional_type = opt;
-    }
-    void value_string(string const& s) override {
-        expect_state(State::AwaitValue);
-        emitter << YAML::Value << s;
-    }
-    void key(string const& s) override {
-        if (state() == State::TypedObject || state() == State::DocumentStart) {
-            emitter << YAML::BeginMap;
-            push_state(State::Map);
-        }
-        expect_state(State::Map);
-        emitter << YAML::Key << s;
-    }
-    void number(double x) override {
-        value_string(std::to_string(x));
-    }
-    void reference(U id) override {
-        emitter << YAML::Alias(V::id_to_string(id));
-    }
 
-protected:
-    void expect_state(State s) {
-        if (s != state()) {
-            throw SerializationError("YAML writer: got state {} while expecting {}."_format((int)state(), (int)s));
+        auto const& node = current().node;
+        if (current().count > 0) {
+            write_link(node);
+            return false;
+        }
+        write_anchor(node);
+
+        auto type_s = node_name(*node);
+        if (!(current().type.is_only() && type_s.find("Value/")==0)) {
+            emitter << YAML::LocalTag(type_s);
+        }
+
+        switch (serialize::NodeWriter::classify(node)) {
+            case serialize::RecordType::Value: {
+                emitter << YAML::Value << serialize::value_to_string(node->static_any());
+                return false;
+            }
+            case serialize::RecordType::Map: {
+                push_state(State::Map);
+                emitter << YAML::BeginMap;
+                return true;
+            }
+            case serialize::RecordType::List: {
+                push_state(State::List);
+                emitter << YAML::BeginSeq;
+                return true;
+            }
+            default:
+                throw serialize::SerializationError("universe imploded");
         }
     }
 
-    void state_mismatch_error(State s) const override {
-        throw DeserializationError("Got state {} while expecting {}"_format((int)state(), (int)s));
+    void object_end() override {
+        switch (state()) {
+            case State::List: {
+                emitter << YAML::EndSeq;
+            } break;
+            case State::Map: {
+                emitter << YAML::EndMap;
+            } break;
+            default:
+                state_mismatch_error(State::Map);
+        }
+        pop_state();
     }
 
-public:
     void flush() {
         pop_state(State::DocumentStart);
         if (!emitter.good())
-            throw SerializationError("YAML Emitter error: {}"_format(emitter.GetLastError()));
+            throw serialize::SerializationError("YAML Emitter error: {}"_format(emitter.GetLastError()));
+    }
+
+private:
+    void state_mismatch_error(State s) const override {
+        throw serialize::SerializationError("Got state {} while expecting {}"_format((int)state(), (int)s));
+    }
+
+    void write_anchor(AbstractReference node) {
+        emitter << YAML::Anchor(id_to_string(*node));
+    }
+
+    void write_link(AbstractReference node) {
+        emitter << YAML::Alias(id_to_string(*node));
     }
 
 private:
     YAML::Emitter emitter;
-    bool optional_type = false;
+    shared_ptr<NodeTree> tree;
 };
 
-} // namespace rainynite::core::serialize
-
-namespace rainynite::core::filters {
 
 void YamlWriter::write_document(std::ostream& output, shared_ptr<Document> document) {
-    auto writer = serialize::YamlCppWriter<serialize::NodeWriter, AbstractReference>(output);
-    writer.object(dynamic_pointer_cast<AbstractValue>(document));
+    auto writer = YamlCppWriter(output);
+    writer.serialize(document);
     writer.flush();
 }
 
